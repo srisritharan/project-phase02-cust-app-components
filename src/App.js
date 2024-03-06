ded745b19a8198fdec5453c0ed109921290c8d2f
srisritharan <srisritharan.s@gmail.com>
2024-03-06 14:10:39 -0500

    Seprate CustomerList Component, added port 3001

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@
 
 import "./App.css";
 import React, { useState, useEffect } from "react";
+import CustomerList from "./components/CustomerList";
 // import customers from "./memdb.js";
 import { getAll, post, put, deleteById } from "./memdb.js";
 
-function log(message) {
+export function log(message) {
   console.log(message);
 }
 
@@ -70,33 +71,11 @@ export function App(params) {
 
   return (
     <div>
-      <div className="boxed">
-        <h4>Customer List</h4>
-        <table id="customer-list">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Pass</th>
-            </tr>
-          </thead>
-          <tbody>
-            {customers.map((item, index) => {
-              return (
-                <tr
-                  key={item.id}
-                  onClick={() => handleListClick(item)}
-                  className={item.id === formObject.id ? "selected" : ""}
-                >
-                  <td>{item.name}</td>
-                  <td>{item.email}</td>
-                  <td>{item.password}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+      <CustomerList
+        customers={customers}
+        formObject={formObject}
+        handleListClick={handleListClick}
+      />
       <div className="boxed">
         <div>
           <h4>{mode}</h4>
---
+++ src/components/CustomerList.js
@@ -0,0 +1,40 @@
+/** @format */
+
+import React, { useState, useEffect } from "react";
+import { getAll, post, put, deleteById } from "../memdb.js";
+import log from "../App.js";
+
+export default function CustomerList(props) {
+  const customers = props.customers;
+  const formObject = props.formObject;
+  const handleListClick = props.handleListClick;
+  return (
+    <div className="boxed">
+      <h4>Customer List</h4>
+      <table id="customer-list">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Pass</th>
+          </tr>
+        </thead>
+        <tbody>
+          {customers.map((item, index) => {
+            return (
+              <tr
+                key={item.id}
+                onClick={() => handleListClick(item)}
+                className={item.id === formObject.id ? "selected" : ""}
+              >
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.password}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
